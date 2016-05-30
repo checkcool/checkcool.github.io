a8459b2a10c5e38f02d88cb260d711b1801b6c64
NelsonKuang <410374308@qq.com>
2016-05-30 10:27:02 +0800

    增加阅读频道

--- src/js/main.js
+++ src/js/main.js
@@ -32,12 +32,15 @@ function indexHandler() {
             util.setHome(this, window.location.href);
         };
         window.onscroll = function () {
-            var headerEl = document.getElementsByTagName("header")[0],
+            var headerEl = document.getElementsByTagName("header")[0],//header shadow
+                rttEl = document.getElementById("rtt"),//return to top
                 scrollTop = document.body.scrollTop;
             if (scrollTop > 0) {
                 util.addClass(headerEl, "shadowed");
+                util.removeClass(rttEl, "hide");
             } else {
                 util.removeClass(headerEl, "shadowed");
+                util.addClass(rttEl, "hide");
             }
         }
     });
